d43a9255e70fa9998aca8d498026cff26c07d317
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-15 20:12:08 +0300

    Created Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+
+export default function Navbar() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            setScrolled(offset > 10);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const navClasses = `sticky top-0 left-0 w-full transition-all duration-300 z-50 ${scrolled ? 'bg-white shadow-md' : 'border-b border-gray-500 bg-transparent'}`;
+    const liClassesActive = `block py-2 px-3 md:p-0 text-white bg-blue-700 rounded-sm md:bg-transparent ${scrolled ? 'md:text-green-smoke-700' : 'bg-white'} `;
+    const liClassesNoActive = `block py-2 px-3 md:p-0 rounded-sm hover:bg-gray-100 md:hover:bg-transparent ${scrolled ? 'md:text-gray-700 md:hover:text-green-smoke-700' : 'text-gray-400 md:hover:text-white'} `;
+    return (
+        <>
+            <nav className={navClasses}>
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a
+                        href="https://flowbite.com/"
+                        className="flex items-center space-x-3 rtl:space-x-reverse"
+                    >
+                        <span className={`self-center text-2xl font-semibold whitespace-nowrap ${scrolled ? 'text-green-smoke-600' : 'text-white'}`}>
+                            DanielKrastev.
+                        </span>
+                    </a>
+                    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                        <button
+                            type="button"
+                            className="text-white bg-green-smoke-600 hover:bg-green-smoke-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+                        >
+                            Contacts
+                        </button>
+                        <button
+                            data-collapse-toggle="navbar-cta"
+                            type="button"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                            aria-controls="navbar-cta"
+                            aria-expanded="false"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 17 14"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M1 1h15M1 7h15M1 13h15"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                    <div
+                        className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                        id="navbar-cta"
+                    >
+                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+                            <li>
+                                <a
+                                    href="#"
+                                    className={liClassesActive}
+                                    aria-current="page"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className={liClassesNoActive}
+                                >
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className={liClassesNoActive}
+                                >
+                                    Skills
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className={liClassesNoActive}
+                                >
+                                    Projects
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
