aa5efe5bb9542278ad2e4d983e9fdda0c6ba1d13
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-23 22:19:27 +0300

    Created express server for portfolio

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { MONGODB_URI_LOCAL, MONGODB_URI_ONLINE } from '../constants.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+//const onlineUri = MONGODB_URI_ONLINE;
+const localUri = MONGODB_URI_LOCAL;
+
+try{
+    await mongoose.connect(onlineUri);
+    console.log("Connected to MongoDB Atlas (Online)");
+}catch(error) {
+    try{
+        await mongoose.connect(localUri);
+        console.log("Connected to Local MongoDB");
+    }catch(localError){
+        console.error("Cannot to connect to any DB!");
+        console.error();
+        process.exit(1); // stop the app
+    }
+}
+
+// Express Setup
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+app.use(express.urlencoded( { extended: false } ));
+
+//route
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is lestening on port ${port} ...`));
