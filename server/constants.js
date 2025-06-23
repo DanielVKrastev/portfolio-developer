3e3de3b479d8cc48a20732ab2667f07fde3f144b
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-23 22:24:34 +0300

    Delete constants.js

--- server/constants.js
+++
@@ -1,3 +0,0 @@
-const dbName = 'daniel-pordolio';
-export const MONGODB_URI_ONLINE = `mongodb+srv://danielkrastev01:KAhuSB4ddbP9vL8C@daniel-cluster.6vnoy1n.mongodb.net/${dbName}`;
-export const MONGODB_URI_LOCAL = `mongodb://127.0.0.1:27017/${dbName}`;
