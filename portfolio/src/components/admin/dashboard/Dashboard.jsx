7cf846cc14876b23184029cd07efab57b4db1830
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-26 22:47:06 +0300

    Update Dashboard.jsx

--- portfolio/src/components/admin/dashboard/Dashboard.jsx
+++ portfolio/src/components/admin/dashboard/Dashboard.jsx
@@ -1,17 +1,34 @@
+import { Link } from 'react-router';
 export default function Dashboard() {
     return (
         <>
-                <h1 className="mb-6 text-3xl font-bold">Welcome to Admin Panel</h1>
-                <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                    <section className="rounded-xl bg-white p-6 shadow dark:bg-gray-800">
-                        <h2 className="mb-2 text-xl font-semibold">Projects</h2>
-                        <p className="text-sm">Add, Update and remove projects.</p>
-                    </section>
-                    <section className="rounded-xl bg-white p-6 shadow dark:bg-gray-800">
-                        <h2 className="mb-2 text-xl font-semibold">Contacts form</h2>
+            <h1 className="mb-6 text-3xl font-bold">Welcome to Admin Panel</h1>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <section className="rounded-xl bg-white p-6 shadow dark:bg-gray-800">
+                    <h2 className="mb-2 text-xl font-semibold">Projects</h2>
+                    <Link to="/admin/projects">
+                    <p className="text-sm">Add, Update and remove projects.</p>
+                    </Link>
+                </section>
+                <section className="rounded-xl bg-white p-6 shadow dark:bg-gray-800">
+                    <h2 className="mb-2 text-xl font-semibold">Skills</h2>
+                    <Link to="/admin/skills">
+                    <p className="text-sm">Add, Update and remove skills.</p>
+                    </Link>
+                </section>
+                <section className="rounded-xl bg-white p-6 shadow dark:bg-gray-800">
+                    <h2 className="mb-2 text-xl font-semibold">Certificates</h2>
+                    <Link to="/admin/certificates">
+                    <p className="text-sm">Add, Update and remove certificates.</p>
+                    </Link>
+                </section>
+                <section className="rounded-xl bg-white p-6 shadow dark:bg-gray-800">
+                    <h2 className="mb-2 text-xl font-semibold">Contacts form</h2>
+                    <Link to="/admin/contacts">
                         <p className="text-sm">See messages.</p>
-                    </section>
-                </div>
+                    </Link>
+                </section>
+            </div>
         </>
     )
 }
