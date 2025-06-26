123fe18b7a83e6aaa900b6e5253ddbafa46d4a16
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-26 22:42:31 +0300

    Created API for Contact messages

---
+++ portfolio/src/api/contactsApi.js
@@ -0,0 +1,34 @@
+import { BASEURL } from "../constants";
+import request from "../utils/requests";
+
+const baseUrl = `${BASEURL}/contact-messages`;
+
+export default {
+    async getAll() {
+        return await request('GET', baseUrl);
+    },
+    async getOne(id){
+        return await request('GET', `${baseUrl}/${id}`);
+    },
+    async create(data){
+        /*
+        const options = {
+        headers: {
+            'X-Authorization': accessToken,
+        }
+        */
+        return await request('POST', baseUrl, data); //TODO: add options
+    },
+    async update(id, data){
+        /*
+        const options = {
+        headers: {
+            'X-Authorization': accessToken,
+        }
+        */
+        return await request('PATCH', `${baseUrl}/${id}`, data); //TODO: add options
+    },
+    async delete(id) {
+        return await request('DELETE', `${baseUrl}/${id}`);
+    }
+}
