84ff15dddd1cc93b3f30de27fe6b03558f4a037d
Daniel Krastev <danielkrastev01@gmail.com>
2025-06-26 22:08:43 +0300

    Added get, create, update and delete

--- portfolio/src/api/skillsApi.js
+++ portfolio/src/api/skillsApi.js
@@ -6,5 +6,29 @@ const baseUrl = `${BASEURL}/tech-stack`;
 export default {
     async getAll() {
         return await request('GET', baseUrl);
+    },
+    async getOne(id){
+        return await request('GET', `${baseUrl}/${id}`);
+    },
+    async create(data){
+        /*
+        const options = {
+        headers: {
+            'X-Authorization': accessToken,
+        }
+        */
+        return await request('POST', baseUrl, data); //TODO: add options
+    },
+    async update(id, data){
+        /*
+        const options = {
+        headers: {
+            'X-Authorization': accessToken,
+        }
+        */
+        return await request('PATCH', `${baseUrl}/${id}`, data); //TODO: add options
+    },
+    async delete(id) {
+        return await request('DELETE', `${baseUrl}/${id}`);
     }
 }
